1109847c5cd0b473af35059b0e1cbc39ea5cbd4d
Gowtham13042007 <papanigowtham@gmail.com>
2025-10-01 13:30:08 +0530

    branch1

---
+++ backend.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template,redirect,url_for
+
+app=Flask(__name__)
+app.secret_key="ab$34"
+
+@app.route("/")
+def index_page():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
